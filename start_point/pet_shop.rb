353d8e8b8b9cd197ff39fa488d890a7a6b582136
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2018-10-19 13:39:08 +0100

    Tests 11/21 passing

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,46 +1,107 @@
 
 #1
-
-
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
 
 #2
 
-
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
 
 #3
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] += amount
+end
 
 #4
 
+# See above
+
+
 #5
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
 
 #6
 
+def increase_pets_sold(pet_shop, amount_sold)
+  pet_shop[:admin][:pets_sold] += amount_sold
+
+end
+
+
 #7
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
 
 #8
 
+def pets_by_breed(pet_shop, breed_wanted)
+  pets_of_breed = []
+  for pet in pet_shop[:pets]
+    pets_of_breed.push(pet) if pet[:breed] == breed_wanted
+  end
+  return pets_of_breed
+end
+
 #9
+
+# As above
 
 #10
 
+def find_pet_by_name(pet_shop, pet_name_wanted)
+    for pet in pet_shop[:pets]
+      return pet if pet[:name] == pet_name_wanted
+    end
+  return nil
+end
+
 #11
+
+# As above
 
 #12
 
+
+
 #13
+
+
 
 #14
 
+
+
 #15
+
+
 
 #16
 
+
+
 #17
+
+
 
 #18
 
+
+
 #19
 
+
+
 #20
+
+
 
 #21
